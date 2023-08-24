631c21bceda5cc021b958ec528d9ed8ae0d0ff4b
Jonathan Monette <jon.monette@gmail.com>
2023-08-23 22:52:59 -0500

    send json

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
                         model="gpt-4", 
                         max_tokens=1000,
                         temperature=0.7, 
-                        messages=[{"role": "user", "content": prompt_template.format(completed_tasks=work[item])}])
+                        messages=[{"role": "user", "content": prompt_template.format(completed_tasks=json.dumps(work[item]))}])
                     output = {}
                     output[f"{item}_content"] = chat_completion.choices[0].message.content
                 summary = ""
