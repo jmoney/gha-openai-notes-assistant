4a43b7b1f94d5487983dde2ca54e961077aea9fd
Jonathan Monette <jon.monette@gmail.com>
2023-08-23 22:49:18 -0500

    concat

--- main.py
+++ main.py
@@ -27,7 +27,8 @@ if __name__ == "__main__":
                     output[f"{item}_content"] = chat_completion.choices[0].message.content
                 summary = ""
                 for value in output:
-                    summary += output[value] + "\n"
+                    summary += output[value]
+                    summary += "\n"
                 print(json.dumps({"content": summary}))
         else:
             print("Unknown assistant", file=sys.stderr)
